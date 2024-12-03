3730855b948448d96763d326ff39ca085d2fea7e
Awointa <akosawointa@gmail.com>
2024-12-03 10:33:19 +0100

    feat(AppLayout): import and render SideNav component

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
+import SideNav from "../components/SideNav";
 
 function AppLayout() {
 	return (
 		<div>
 			<Header />
+			<SideNav />
 		</div>
 	);
 }
